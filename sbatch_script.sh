ef7b20354792edcc6ab507b1b49b84b3205e4287
s-mazza <simone.mazzacano@gmail.com>
2026-04-17 17:12:53 +0200

    exclude deeplearn2 as node

--- sbatch_script.sh
+++ sbatch_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sbatch -N 1 --gpus=nvidia_geforce_rtx_3090:1 run_docker.sh
+sbatch -N 1 --gpus=nvidia_geforce_rtx_3090:1 -x deeplearn2 run_docker.sh
 
 # To resume from checkpoint:
-# sbatch -N 1 --gpus=nvidia_geforce_rtx_3090:1 run_docker.sh --resume
+# sbatch -N 1 --gpus=nvidia_geforce_rtx_3090:1 -x deeplearn2 run_docker.sh --resume
 
 # If your dataset is too large to replicate on all nodes, pin to faretra:
 # sbatch -N 1 --gpus=nvidia_geforce_rtx_3090:1 -w faretra run_docker.sh

1f3d2503d357e572808d68295e43560007439748
s-mazza <simone.mazzacano@gmail.com>
2026-04-23 21:59:50 +0200

    add run_eval.sh

---
+++ run_eval.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PHYS_DIR="/home/mazzacano/embedding-inversion-demo"
+LLM_CACHE_DIR="/llms"
+
+docker run \
+    -v "$PHYS_DIR":/workspace \
+    -v "$LLM_CACHE_DIR":/llms \
+    -e HF_HOME="/llms" \
+    --rm \
+    --gpus '"device=0"' \
+    emb-inversion \
+    python3 /workspace/eval_quality.py \
+        --checkpoint /workspace/checkpoints_v3_mmbert_jinav3/best.pt \
+        "$@"

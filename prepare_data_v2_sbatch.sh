e5b29f2da25fe59fb2d8b48b816aa3b30fd718f6
s-mazza <simone.mazzacano@gmail.com>
2026-04-27 11:16:30 +0200

    data prep v2: add expandable_segments (keep batch 512)

--- prepare_data_v2_sbatch.sh
+++ prepare_data_v2_sbatch.sh
@@ -3,6 +3,7 @@ docker run \
     -v /home/mazzacano/embedding-inversion-demo:/workspace \
     -v /llms:/llms \
     -e HF_HOME=/llms \
+    -e PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True \
     --rm --memory=30g \
     --gpus '"device='"$CUDA_VISIBLE_DEVICES"'"' \
     emb-inversion /workspace/prepare_data_stage2_v2.sh
